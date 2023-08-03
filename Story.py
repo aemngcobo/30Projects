97fd6e53e8a73fd38df14e647b55fbd345e33fc9
Anele <ngcobo.anele@gmail.com>
2023-08-03 08:21:50 +0200

    Created initial layout for the story

---
+++ Story.py
@@ -0,0 +1,11 @@
+def get_input(word_type: str):
+    user_input: str =input(f"Enter a {word_type}:")
+    return user_input
+
+Noun1= get_input("noun")
+Adj1=get_input("Adj")
+Verb1= get_input("verb")
+Noun2= get_input("noun")
+Verb2= get_input("verb")
+
+Story= f"""
